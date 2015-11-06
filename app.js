61833066ad4819ef7475ce0dcffda03d6325409e
root <root@pdocker.(none)>
2015-11-06 09:54:24 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var fs = require('fs');
+var htmlFile = "index.html";
+
+var app = express();
+
+/* serving static file */
+app.use('/css', express.static('css'));
+app.use('/js', express.static('js'));
+app.use('/img', express.static('img'));
+
+/* main page */
+app.get('/', function(request, response) {
+    var buffer = fs.readFileSync(htmlFile);
+    var content = buffer.toString();
+    response.send(content);
+});
+
+/* download pages */
+app.get('/downloads', function(req, res){
+	var buffer = fs.readFileSync("pages/downloads.html");
+	var content = buffer.toString();
+	res.send(content);
+});
+
+/* about page */
+app.get('/about', function(req, res){
+	var buffer = fs.readFileSync("pages/about.html");
+	var content = buffer.toString();
+	res.send(content);
+});
+
+/* contact page */
+app.get('/contact', function(req, res){
+	var buffer = fs.readFileSync("pages/contact.html");
+	var content = buffer.toString();
+	res.send(content);
+});
+
+/* listen the server at port 8080 */
+var port = process.env.PORT || 8080;
+var server = app.listen(port, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log("Server Listening at http://%s:%s ", host, port);
+});
